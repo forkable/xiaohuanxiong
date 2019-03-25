ec93a00735ba3330e68017f880041acb712a54bc
hiliqi <4780044@qq.com>
2019-03-25 18:02:59 +0800

    增加清理缓存api

--- application/api/controller/Common.php
+++ application/api/controller/Common.php
@@ -8,8 +8,20 @@
 
 namespace app\api\controller;
 
+use think\facade\App;
+use think\facade\Cache;
+use think\Controller;
 
-class Common
+class Common extends Controller
 {
-
+    public function clearcache(){
+        $key = input('api_key');
+        if (empty($key) || is_null($key)){
+            return 'api密钥错误！';
+        }
+        Cache::clear('redis');
+        $rootPath = App::getRootPath();
+        delete_dir_file($rootPath . '/runtime/cache/') && delete_dir_file($rootPath . '/runtime/temp/');
+        return '清理成功';
+    }
 }
