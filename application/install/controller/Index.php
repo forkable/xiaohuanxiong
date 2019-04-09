ab2e1b8f34183ae181a1e43b24af5adf9513e0e4
hiliqi <4780044@qq.com>
2019-04-09 13:12:59 +0800

    1

--- application/install/controller/Index.php
+++ application/install/controller/Index.php
@@ -389,7 +389,7 @@ return [
     // 数据库编码默认采用utf8
     'charset'         => 'utf8',
     // 数据库表前缀
-    'prefix'          => '{$data['prefix']}',
+    'prefix'          => '{$data['prefix']}_',
     // 数据库调试模式
     'debug'           => false,
     // 数据库部署方式:0 集中式(单一服务器),1 分布式(主从服务器)
